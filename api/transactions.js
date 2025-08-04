15272e31b81bc6b3dfaa1f4d7bd64bfac6aa2a5f
Shariar-hash <montasirmajumdar123@gmail.com>
2025-08-05 04:28:51 +0600

    Force redeployment - add comment to transactions endpoint

--- api/transactions.js
+++ api/transactions.js
@@ -1,6 +1,7 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
+// Transactions API endpoint - handles CRUD operations for user transactions
 module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
